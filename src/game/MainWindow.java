4e99f781f4f696dffeff50c6842fadb1b4afe49d
Cr1tError <maksym.kokora@gmail.com>
2022-02-13 12:53:40 +0100

    we made a MainWindow class and set some setting to game window

---
+++ src/game/MainWindow.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+        public MainWindow(){
+            setTitle("Змейка");
+            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            setSize(320, 345);
+            setLocation(400, 400);
+            setVisible(true);
+        }
+
+    public static void main(String[] args) {
+        MainWindow mainwindow  = new MainWindow();
+    }
+}
